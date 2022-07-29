ef4d05d936d24d0ec87c652a886dac87d8c40ba2
anastasiiasok <anastasiiasok@gmail.com>
2022-07-29 11:55:39 -0400

    feat: change theme

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,31 @@
-const num: number = 1;
+const headerThemeBtnNode: HTMLDivElement | null = document.querySelector('.header__dark-mode-wrapper');
 
-console.log(num);
+function isTheme(): 'theme-light' | 'theme-dark' {
+	if (document.documentElement.classList.contains('theme-light')) {
+		return 'theme-light';
+	} else if ((document.documentElement.classList.contains('theme-dark'))) {
+		return 'theme-dark';
+	}
+	return 'theme-light';
+}
+
+function changeTheme(lastTheme: string, nextTheme: string): void {
+	document.documentElement.classList.remove(lastTheme);
+	document.documentElement.classList.add(nextTheme);
+}
+
+function handlerClickEventHeaderThemeBtn(event: MouseEvent): void {
+	const theme = isTheme();
+
+	if (theme === 'theme-light') {
+		changeTheme('theme-light', 'theme-dark');
+	} else if (theme === 'theme-dark') {
+		changeTheme('theme-dark', 'theme-light');
+	}
+
+}
+
+headerThemeBtnNode?.addEventListener('click', handlerClickEventHeaderThemeBtn);
+
+
+
